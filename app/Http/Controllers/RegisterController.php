a54223d32b2147bcbab97f41b580856d6df4caa2
Ramdoni <doni.enginer@gmail.com>
2018-10-24 20:23:31 +0700

    Update

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 
-use App\ModelUser;
+use App\Models\ModelUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
